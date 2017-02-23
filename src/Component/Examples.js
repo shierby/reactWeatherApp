a15ae1726032a06ab77229db5a78a3779392650f
shierby <shierby@gmail.com>
2017-02-23 16:23:58 +0200

    Rename example page

--- src/Component/Examples.js
+++ src/Component/Examples.js
@@ -4,7 +4,7 @@ class Examples extends Component {
   render() {
     return (
       <div className="example">
-        <h3>Examples Component!</h3>
+        <h3>Examples</h3>
         <p>Welcome to example page!</p>
       </div>
     )
