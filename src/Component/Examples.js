e7a89c9ce08ed5adf723369f244a28c68214416e
shierby <shierby@gmail.com>
2017-02-23 16:06:51 +0200

    added copy

--- src/Component/Examples.js
+++ src/Component/Examples.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Examples extends Component {
   render() {
     return (
-      <div>
+      <div className="example">
         <h3>Examples Component!</h3>
+        <p>Welcome to example page!</p>
       </div>
     )
   }
