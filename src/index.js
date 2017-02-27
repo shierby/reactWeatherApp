6cd09db24c4b442834315fca8b98933c749411cc
shierby <shierby@gmail.com>
2017-02-27 12:55:17 +0200

    add foundation & jQuery

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {Route, Router, IndexRoute,browserHistory} from 'react-router';
+import { Router, browserHistory } from 'react-router';
+import $ from 'jquery';
+
+//load foundation
+import 'foundation-sites/dist/css/foundation.min.css'
+import 'foundation-sites/dist/js/foundation.min'
+$(document).foundation();
 
 import './index.css';
 
-import Weather from './Component/Weather'
-import About from './Component/About'
-import Examples from './Component/Examples'
 import routes from './routes'
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    {/*<Route path="/" component={App}>
-      <Route path="/about" component={About} />
-      <Route path="/examples" component={Examples} />
-      <IndexRoute component={Weather}/>
-    </Route>*/}
     {routes}
   </Router>,
   document.getElementById('root')
